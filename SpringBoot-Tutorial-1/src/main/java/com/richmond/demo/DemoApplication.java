88d6ccd02b43fbdc5bbbbd7442f319236f915cf3
Richmond Tan <richmondtanch@gmail.com>
2020-01-22 00:12:19 +0800

    stop @ 4

--- SpringBoot-Tutorial-1/src/main/java/com/richmond/demo/DemoApplication.java
+++ SpringBoot-Tutorial-1/src/main/java/com/richmond/demo/DemoApplication.java
@@ -1,7 +1,11 @@
 package com.richmond.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +13,18 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Controller
+	public class SimpleController {
+		@Value("${spring.application.name}")
+		String appName;
+
+		@GetMapping("/")
+		public String homePage(Model model) {
+			model.addAttribute("appName", appName);
+			return "home";
+		}
+	}
+
 
 }
